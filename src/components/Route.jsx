439f66c606b63445d2ed207fb8438d4ae4c046fc
Jerry Kwong <jerry_kcy@hotmail.com>
2024-01-22 16:41:06 +0800

    Added functionality for popup on click: - Added styling for the popup in Map.css - Added binding of popup to layers

--- src/components/Route.jsx
+++ src/components/Route.jsx
@@ -18,15 +18,54 @@ function Route() {
 
   useEffect(() => {
     if (appState.geoJson) {
-      console.log(appState.geoJson);
+      console.log("============= Viewing routes =============");
+      // Get container
       const container = context.layerContainer || context.map;
-      const geoJsonLayer = new L.geoJSON(appState.geoJson);
+      console.log("Retrieved map container", container);
+
+      // Create route layer
+      console.log("Creating layer from GeoJson:", appState.geoJson);
+      const geoJsonLayer = new L.geoJSON(appState.geoJson).bindPopup(layer => {
+        console.log(layer.feature);
+        const popupContent = document.createElement("div");
+        popupContent.innerHTML = `
+          <table class="popup-table">
+            <thead>
+              <tr>
+                <td>Property</td>
+                <td>Value</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Vehicle Ref.</td>
+                <td>${layer.feature.properties.VehicleRef}</td>
+              </tr>
+              <tr>
+                <td>Origin</td>
+                <td>${layer.feature.properties.OriginName}</td>
+              </tr>
+              <tr>
+                <td>Destination</td>
+                <td>${layer.feature.properties.DestinationName}</td>
+              </tr>
+            </tbody>
+          </table>
+        `;
+        return popupContent;
+      });
+      console.log("Created layer:", geoJsonLayer);
+
+      // Update route to component state
       updateRoute(draft => {
         draft.geoJsonLayer = geoJsonLayer;
       });
+
+      // Add layer to map
       container.addLayer(geoJsonLayer);
 
-      console.log("=========== Fitting bounds ===========");
+      // Create bounds for current set of routes
+      // and center map on bounds with animation
       const bboxArray = bbox(appState.geoJson);
       const corner1 = [bboxArray[1], bboxArray[0]];
       const corner2 = [bboxArray[3], bboxArray[2]];
