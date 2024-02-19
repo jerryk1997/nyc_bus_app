fe02f487657808dc45efc29b935120aea335e04a
Jerry Kwong <jerry_kcy@hotmail.com>
2024-02-19 09:40:02 +0800

    Refactor: Zoom control - Removed zoom control

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -50,7 +50,11 @@ function Map() {
       >
         <CircularProgress color="inherit" />
       </Backdrop>
-      <MapContainer center={appState.newYorkCoordinates} zoom={13}>
+      <MapContainer
+        center={appState.newYorkCoordinates}
+        zoom={13}
+        zoomControl={false}
+      >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
